bed3cc127fd7d398265f2ebaa14f8246156b25e1
SHY <shy9729@gmail.com>
2025-12-24 11:32:58 +0900

    ✨ feat: Lv_2. 코드 추가 퀴즈 - JWT의 이해
    23일자로 한건 auth에도 nickname 추가해서 작성했는데 이게 맞는지 확인하기
    
    -> AuthService, CommentService, ManagerService, SignupRequest, TodoService, User, UserResponse, UserService
    에 nickname 추가

--- src/main/java/org/example/expert/domain/user/entity/User.java
+++ src/main/java/org/example/expert/domain/user/entity/User.java
@@ -17,12 +17,15 @@ public class User extends Timestamped {
     private Long id;
     @Column(unique = true)
     private String email;
+    @Column(name = "nickname", unique = false)
+    private String nickname;
     private String password;
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
 
-    public User(String email, String password, UserRole userRole) {
+    public User(String email, String nickname, String password, UserRole userRole) {
         this.email = email;
+        this.nickname = nickname;
         this.password = password;
         this.userRole = userRole;
     }
